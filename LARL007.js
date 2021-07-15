b53d398c7be6f97b0b0960cfd8faa465fb00c540
EltioLARL <87486801+LuisLuna00798u5498t548gh8587@users.noreply.github.com>
2021-07-15 11:07:24 -0500

    Create LARL007.js

---
+++ LARL007.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<script type="text/javascript">
+<!--
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}
+//-->
+</script>
+</head>
+
+<body>
+<form name="form" id="form">
+  <select name="jumpMenu" id="jumpMenu" onchange="MM_jumpMenu('parent',this,0)">
+    <option value="menu.html">Menu1</option>
+    <option value="menu2.html">Menu2</option>
+    <option value="menu3.html">Menu3</option>
+  </select>
+</form>
+</body>
+</html>
